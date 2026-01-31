404ea10fba12200f82310564e20f8ce5318d57f6
SeanBohne <149605219+SeanBohne@users.noreply.github.com>
2026-01-31 17:26:06 -0500

    Update cartesian_pert.m

--- +propagate/cartesian_pert.m
+++ +propagate/cartesian_pert.m
@@ -2,9 +2,8 @@ function dxdt = cartesian_pert(t, x, param)
 % This function propogates cartesian states using two-body motion under
 % various perturbations
 
-
 dxdt = zeros(6, 1);
-mu = param.mu_earth;
+mu = param.mu_grav_main;
 
 r = norm(x(1:3)); % Distance between orbited body and satellite (km)
 
@@ -55,5 +54,14 @@ if isfield(param, 'use_Drag') && param.use_Drag
     w_earth = param.w_earth;
     [~, a_Drag] = propagate.Drag_pert(r_vec, v_vec, rho_ref, dens_scale, h0, CD, A_m, w_earth); % Drag perturbation accelerations (km/s^2)
     dxdt(4:6) = dxdt(4:6) + a_Drag;
+end
+
+if isfield(param, 'use_3body') && param.use_3body
+    % Extract needed parameters
+    GM_star = param.GM_star;
+    l_star = param.l_star;
+    mu_grav = param.mu_grav;
+    [~, a_moon] = propagate.moon_pert(t, r_vec, v_vec, GM_star, l_star, mu_grav); % Moon perturbation accelerations (km/s^2)
+    dxdt(4:6) = dxdt(4:6) + a_moon;
 end
 end
